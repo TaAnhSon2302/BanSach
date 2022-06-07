e82a66f3e74a44b9b15b431a4b76a5c85ded20cd
Admin <2051060687@e.tlu.edu.vn>
2022-06-07 21:49:27 +0700

    Add project files.

---
+++ BanSach/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace BanSach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbIsSV = new System.Windows.Forms.CheckBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtQuanity = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnTT = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnTK = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtKH = new System.Windows.Forms.TextBox();
+            this.txtSV = new System.Windows.Forms.TextBox();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Font = new System.Drawing.Font("Arial", 15F);
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHƯƠNG TRÌNH TÍNH TIỀN BÁN SÁCH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên Khách Hàng:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTotal);
+            this.groupBox1.Controls.Add(this.txtQuanity);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.cbIsSV);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 83);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(365, 140);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hóa đơn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số lượng Sách:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 14);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Khách hàng là SV:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 14);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Thành tiền:";
+            // 
+            // cbIsSV
+            // 
+            this.cbIsSV.AutoSize = true;
+            this.cbIsSV.Location = new System.Drawing.Point(118, 78);
+            this.cbIsSV.Name = "cbIsSV";
+            this.cbIsSV.Size = new System.Drawing.Size(15, 14);
+            this.cbIsSV.TabIndex = 5;
+            this.cbIsSV.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(118, 26);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(241, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtQuanity
+            // 
+            this.txtQuanity.Location = new System.Drawing.Point(118, 52);
+            this.txtQuanity.Name = "txtQuanity";
+            this.txtQuanity.Size = new System.Drawing.Size(241, 20);
+            this.txtQuanity.TabIndex = 7;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Location = new System.Drawing.Point(118, 94);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(241, 20);
+            this.txtTotal.TabIndex = 8;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnEnd);
+            this.groupBox2.Controls.Add(this.btnTK);
+            this.groupBox2.Controls.Add(this.btnNext);
+            this.groupBox2.Controls.Add(this.btnTT);
+            this.groupBox2.Location = new System.Drawing.Point(16, 244);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(355, 39);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnTT
+            // 
+            this.btnTT.Location = new System.Drawing.Point(14, 10);
+            this.btnTT.Name = "btnTT";
+            this.btnTT.Size = new System.Drawing.Size(75, 23);
+            this.btnTT.TabIndex = 0;
+            this.btnTT.Text = "Tính TT";
+            this.btnTT.UseVisualStyleBackColor = true;
+            this.btnTT.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(95, 10);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "Tiếp";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnTK
+            // 
+            this.btnTK.Location = new System.Drawing.Point(184, 10);
+            this.btnTK.Name = "btnTK";
+            this.btnTK.Size = new System.Drawing.Size(75, 23);
+            this.btnTK.TabIndex = 2;
+            this.btnTK.Text = "Thống kê";
+            this.btnTK.UseVisualStyleBackColor = true;
+            this.btnTK.Click += new System.EventHandler(this.btnTK_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(265, 10);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 3;
+            this.btnEnd.Text = "Kết thúc";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtKH);
+            this.groupBox3.Controls.Add(this.txtSV);
+            this.groupBox3.Controls.Add(this.txtSum);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(30, 305);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(341, 105);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Thống kê";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 14);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Tổng số KH:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 39);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 14);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Tổng Số KH là SV:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 14);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Tổng doanh thu:";
+            // 
+            // txtKH
+            // 
+            this.txtKH.Enabled = false;
+            this.txtKH.Location = new System.Drawing.Point(100, 10);
+            this.txtKH.Name = "txtKH";
+            this.txtKH.Size = new System.Drawing.Size(235, 20);
+            this.txtKH.TabIndex = 9;
+            // 
+            // txtSV
+            // 
+            this.txtSV.Enabled = false;
+            this.txtSV.Location = new System.Drawing.Point(100, 36);
+            this.txtSV.Name = "txtSV";
+            this.txtSV.Size = new System.Drawing.Size(235, 20);
+            this.txtSV.TabIndex = 10;
+            // 
+            // txtSum
+            // 
+            this.txtSum.Enabled = false;
+            this.txtSum.Location = new System.Drawing.Point(100, 64);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.Size = new System.Drawing.Size(235, 20);
+            this.txtSum.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnTT;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 443);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtQuanity;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.CheckBox cbIsSV;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnTK;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnTT;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtKH;
+        private System.Windows.Forms.TextBox txtSV;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ BanSach/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BanSach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int Standard = 0;
+        int IsSV = 0;
+        double TotalInCome = 0;
+        SqlConnection cn;
+        SqlCommand cmd;
+        string sql = @"Data Source=H114\SQLEXPRESS;Initial Catalog=Pratice;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+
+
+        void loadata()
+        {
+           cmd=cn.CreateCommand();
+           cmd.CommandText = "Select * from ThongKe";
+           adapter.SelectCommand = cmd;
+           dt.Clear();
+           adapter.Fill(dt);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("Yêu cầu nhập tên");
+            }
+            if (txtQuanity.Text == "" && txtName.Text!="")
+            {
+                MessageBox.Show("Yêu cầu nhập số nguyên dương");
+            }
+            if (txtQuanity.Text != "")
+            {
+                try
+                {
+                    int a = int.Parse(txtQuanity.Text);
+                    if (a < 0)
+                        MessageBox.Show("Yêu cầu là số nguyên dương");
+                    else
+                    {
+                        Standard += 1;
+                        double NormalPrice = 200000 * a;
+                        double SVPrice = 200000 * a  - (0.05*20000*a);
+                        if(cbIsSV.Checked)
+                        {
+                            txtTotal.Text = SVPrice.ToString();
+                            IsSV += 1;
+                            TotalInCome += NormalPrice;
+                        }
+                        else
+                        {
+                            txtTotal.Text = NormalPrice.ToString();
+                            TotalInCome += SVPrice;
+                        }    
+                    }    
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Yêu cầu là số nguyên dương");
+                }
+            }
+            
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            foreach(Control c in this.groupBox1.Controls)
+            {
+                if(c is TextBox)
+                {
+                    c.Text = "";
+                }  
+                
+            }
+            cbIsSV.Checked = false;
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Bạn có muốn thoát", "Thông báo", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+                Application.ExitThread();
+        }
+
+        private void btnTK_Click(object sender, EventArgs e)
+        {
+            txtKH.Text = Standard.ToString();
+            txtSV.Text = IsSV.ToString();
+            txtSum.Text = TotalInCome.ToString();
+            cn = new SqlConnection(sql);
+            cn.Open();
+            cmd =cn.CreateCommand();
+            cmd.CommandText = "insert into ThongKe(TongSoKH, LaSV, InCome) values ('" + txtKH.Text + "','" + txtSV.Text + "','" + txtSum.Text + "')";
+            cmd.ExecuteNonQuery();
+            loadata();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
